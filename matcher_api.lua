0eed79661755bea2c6a4a49b30d5e2a8627bc1d3
Hugo Duksis <duksis@gmail.com>
2018-04-17 12:08:07 +0200

    Shuffle payload from files in data folder

--- matcher_api.lua
+++ matcher_api.lua
@@ -1,27 +1,56 @@
 local headers = {}
 headers['Content-Type'] = 'application/json'
 
-local function read_file(file)
+function read_file(file)
   local f = assert(io.open(file, "rb"))
   local content = f:read("*all")
   f:close()
   return content
 end
 
-local function render(template, env)
+function read_files(file_paths)
+  local files = {}
+  for _,file_path in ipairs(file_paths) do
+    table.insert(files, read_file(file_path))
+  end
+  return files
+end
+
+function ls(dir)
+  local file_paths = {}
+  local p = io.popen('find "'..dir..'" -type f')
+  for file_path in p:lines() do
+    table.insert(file_paths, file_path)
+  end
+  return file_paths
+end
+
+function len(list)
+  local count = 0
+  for _ in pairs(list) do
+    count = count + 1
+  end
+  return count
+end
+
+function render(template, env)
   return string.format(template, unpack(env))
 end
 
-local positions = {'./data/enriched/positions/elixir.json'}
-local resumes = {'./data/enriched/resumes/elixir.json'}
+local position_paths = ls('./data/enriched/positions')
+local positions = read_files(position_paths)
+local position_count = len(position_paths)
+local resume_paths = ls('./data/enriched/resumes')
+local resumes = read_files(resume_paths)
+local resume_count = len(resume_paths)
 
 local template = '{"resume": %s, "position": %s}'
 
 request = function()
   local path = "/match"
 
-  local resume = read_file(resumes[1])
-  local position = read_file(positions[1])
+  local resume = resumes[math.random(1, resume_count)]
+  local position = positions[math.random(1, position_count)]
   local bindings = {resume, position}
   local body = render(template, bindings)
   return wrk.format("POST", path, headers, body)
